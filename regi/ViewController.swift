b4cac8ae0234a5e82fca3535a21e7c9fc97015f0
gtech <gtech@macbook-test-04.local>
2018-07-24 16:38:57 +0800

    Initial Commit

---
+++ regi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  regi
+//
+//  Created by gtech on 2018/7/24.
+//  Copyright © 2018年 gtech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
